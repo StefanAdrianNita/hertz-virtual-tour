eff707671f056fd437b8096db1118e9f71e4ede0
Manuel Marongiu <50673709+Manuel4twenty@users.noreply.github.com>
2021-01-15 12:51:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,6 @@
       scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
     });
 
-    const purposeLink=['/','/','/','/','/','/','/','/','/','/','/','/','/','https://youtu.be/x1-pRGldWbM'];
 
     // Create info hotspots.
     data.infoHotspots.forEach(function(hotspot) {
